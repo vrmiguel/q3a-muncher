e41a6f9fd81a8bd227d167c6b9609b608d4bca31
Vinícius Rodrigues Miguel <lemao.vrm07@hotmail.com>
2022-08-02 17:15:24 -0300

    feat: add `ReallocBufReader`

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ mod parser;
 use std::{
     fs::File,
     io::{BufRead, BufReader},
+    path::Path,
 };
 
 pub use cause_of_death::{CauseOfDeath, CAUSES_OF_DEATH};
@@ -17,13 +18,11 @@ fn run() -> Result<()> {
     let path =
         std::env::args_os().nth(1).ok_or(Error::MissingFile)?;
 
-    let file = File::open(path)?;
-    let reader = BufReader::new(file);
+    let mut reader = ReallocBufReader::from(path)?;
     let mut parser = LogParser::new();
 
-    for line in reader.lines() {
-        let line = line?;
-        parser.parse_line(&line)?;
+    while let Some(line) = reader.read_line()? {
+        parser.parse_line(line)?;
     }
 
     Ok(())
@@ -32,5 +31,29 @@ fn run() -> Result<()> {
 fn main() {
     if let Err(err) = run() {
         eprintln!("{err}");
+    }
+}
+
+struct ReallocBufReader {
+    reader: BufReader<File>,
+    buffer: String,
+}
+
+impl ReallocBufReader {
+    pub fn from<P: AsRef<Path>>(path: P) -> Result<Self> {
+        let file = File::open(path)?;
+        let reader = BufReader::new(file);
+        let buffer = String::with_capacity(1024);
+
+        Ok(Self { reader, buffer })
+    }
+
+    pub fn read_line(&mut self) -> Result<Option<&str>> {
+        self.buffer.clear();
+
+        let bytes_read =
+            self.reader.read_line(&mut self.buffer)?;
+
+        Ok((bytes_read != 0).then(|| self.buffer.as_str()))
     }
 }
